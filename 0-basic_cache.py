d5eafd50f584d3593bd16d8f195c4e9f7a8d2549
junior sibusiso ngondo <juniorthemba1@gmail.com>
2024-01-03 20:43:57 +0000

    commit

--- 0-basic_cache.py
+++ 0-basic_cache.py
@@ -9,13 +9,10 @@ class BasicCache(BaseCaching):
     def put(self, key, item):
         """ Add an item in the cache """
         if key and item:
-            # If both key and item are provided, add the item to the cache
             self.cache_data[key] = item
 
     def get(self, key):
         """ Get an item by key """
         if key and key in self.cache_data:
-            # If the key is provided and exists in the cache
             return self.cache_data.get(key)
-        # If the key is not provided or doesn't exist in the cache, return None
         return None
